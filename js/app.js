083f3529577a5ef17d8fa010486afe307f372c4d
zarrin-tasnim <zarrintasnim200420@gmail.com>
2022-09-03 19:34:16 +0600

    card dynamic added

--- js/app.js
+++ js/app.js
@@ -22,11 +22,76 @@ const displayNews = (news) => {
         newsList.classList.add('nav-link');
         newsList.innerHTML = `
         
-            <a class="nav-link" href="#">${element.category_name}</a>
+            <a id="categoryName" class="nav-link" href="#" onclick="loadNewsDEtails(${element.category_id})">${element.category_name}</a>
         `;
         newsCategoryContainer.appendChild(newsList);
     });
 
+
+}
+
+//clicking news category show details
+const loadNewsDEtails = async (id) => {
+    try {
+        const url = `https://openapi.programming-hero.com/api/news/category/0${id}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        displayNewsDetails(data.data);
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+
+}
+
+const displayNewsDetails = (newsDetails) => {
+
+
+
+    const categoryName = document.getElementById('categoryName');
+    const categoryNameText = categoryName.innerText;
+    const categoryDetailsContainer = document.getElementById('card-container');
+    categoryDetailsContainer.textContent = '';
+    newsDetails.forEach(element => {
+
+        console.log(element);
+
+        const card = document.createElement('div');
+        card.classList.add("card", "mb-3", "rounded");
+        card.innerHTML = `
+            <div class="card mb-3 rounded" style="max-width: 800px;">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="${element.thumbnail_url}" class="img-fluid rounded" alt="...">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">${element.title}</h5>
+                                    <p class="card-text text-muted"
+                                        style="max-width: 290px;">${element.details.length > 50 ? element.details.slice(0, 100) + '...' : element.details}
+                                        </p>
+                                    <div class="row d-flex justify-content-between align-items-center p-2 mt-3">
+                                        <div class="col-5 d-flex justify-content-start align-items-center">
+                                            <img src="${element.author.img}" class="rounded-circle me-2" alt="..." style="width: 50px;">
+                                            <h6 id="author'sName">${element.author.name}</h6>
+                                        </div>
+                                        <div class="col-3  d-flex justify-content-center align-items-center ">
+                                            <i class="fa-regular fa-eye p-1"></i>
+                                            <h6 id="viewarsNUmber" class="p-2 mt-1">${element.total_view}</h6>
+                                        </div>
+                                         <div class="col-4  d-flex justify-content-center align-items-center ">
+                                            <i class="fa-regular fa-eye p-1"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        `;
+        categoryDetailsContainer.appendChild(card);
+
+    });
 
 }
 
